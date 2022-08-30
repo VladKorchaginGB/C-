18f94ef90a53a43bea8f356198d811ea5cd29f10
Korchagin <rag.slava@mail.ru>
2022-08-30 23:07:29 +0300

    add Example006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,27 @@
+﻿int kettlebell1 = 1;
+int kettlebell2 = 8;
+int kettlebell3 = 3;
+int kettlebell4 = 2;
+int kettlebell5 = 6;
+int max = kettlebell1;
+if (kettlebell1 > max)
+{
+    max = kettlebell1;
+}
+if (kettlebell2 > max)
+{
+    max = kettlebell2;
+}
+if (kettlebell3 > max)
+{
+    max = kettlebell3;
+}
+if (kettlebell4 > max)
+{
+    max = kettlebell4;
+}
+if (kettlebell5 > max)
+{
+    max = kettlebell5;
+}
+Console.Write(max);

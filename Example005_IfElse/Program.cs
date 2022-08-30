d192783aa2e0be56d88974a85c47ab1ad1d7da51
Korchagin <rag.slava@mail.ru>
2022-08-30 22:55:37 +0300

    add Example005 If Else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "влад")
+{
+    Console.WriteLine("Ура, это же Влад!!!");
+}
+else
+{
+    Console.WriteLine("Добрый день, ");
+    Console.WriteLine(username);
+}

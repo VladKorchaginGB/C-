6528b3e940c3dfa8640214b2c5d5673101497c04
Korchagin <rag.slava@mail.ru>
2022-09-03 23:15:48 +0300

    add Homework008

---
+++ Homework008/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+string N_string = Console.ReadLine();
+int N = Convert.ToInt32(N_string);
+if (N % 2 == 0)
+{
+    while (N >= 1)
+    {
+        Console.WriteLine(N);
+        N = N - 2;
+    }
+}
+else
+{
+    N = N - 1;
+    while (N >= 1)
+    {
+        Console.WriteLine(N);
+        N = N - 2;
+    }
+}

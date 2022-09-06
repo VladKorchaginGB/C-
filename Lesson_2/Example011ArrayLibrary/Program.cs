6aaaa7280b18068c77825ec758c3d8a3ef2081a4
Korchagin <rag.slava@mail.ru>
2022-09-06 12:50:29 +0300

    add Example011, 3 function

---
+++ Lesson_2/Example011ArrayLibrary/Program.cs
@@ -0,0 +1,43 @@
+﻿void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int IndexOF(int[] collection, int find)
+{
+    int index = 0;
+    int lenght = collection.Length;
+    int position = 0;
+    while (index < lenght)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = IndexOF(array, 4);
+Console.WriteLine(pos);

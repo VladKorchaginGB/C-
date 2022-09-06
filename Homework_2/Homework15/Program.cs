cad9e39c18de58de7e621a28fc8c666889567808
Korchagin <rag.slava@mail.ru>
2022-09-06 14:29:01 +0300

    add Homework15

---
+++ Homework_2/Homework15/Program.cs
@@ -0,0 +1,17 @@
+﻿void Weekand(int day)
+{
+    int[] week = new int[7] { 1, 2, 3, 4, 5, 6, 7 };
+    if (day == week[5])
+    {
+        Console.WriteLine("ДА");
+    }
+    else if (day == week[6])
+    {
+        Console.WriteLine("ДА");
+    }
+    else
+    {
+        Console.WriteLine("НЕТ");
+    }
+}
+Weekand(6);

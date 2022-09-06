f9b796c0eac37501d0d6be6655872438c7f30bee
Korchagin <rag.slava@mail.ru>
2022-09-06 13:01:56 +0300

    add Homework_2 > Homework_10

---
+++ Homework_2/Homework_10/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+int SecondNumber(int num)
+{
+    num = num / 10;
+    num = num % 10;
+    return num;
+}
+int test = SecondNumber(486);
+Console.WriteLine(test);

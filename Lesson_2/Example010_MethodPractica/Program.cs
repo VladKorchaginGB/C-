b0013769e9ba497138be411c6899e0a3a3a79323
Korchagin <rag.slava@mail.ru>
2022-09-06 12:07:20 +0300

    add lesson2

---
+++ Lesson_2/Example010_MethodPractica/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 17, 21, 132, 43, 547, 65, 47, 381 };
+
+int n = array.Length;
+int find = 132;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}

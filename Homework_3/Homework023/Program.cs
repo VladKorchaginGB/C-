9e784afa79040ad193a6d83380ff269e89642142
Korchagin <rag.slava@mail.ru>
2022-09-07 23:22:39 +0300

    Сделал 23 задание

---
+++ Homework_3/Homework023/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+void TablCube(double N)
+{
+    for (int i = 1; i < N + 1; i++)
+    {
+        double result = Math.Pow(i, 3);
+        Console.WriteLine(result);
+    }
+}
+TablCube(4);

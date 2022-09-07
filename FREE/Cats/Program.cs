3db4478ef98637e35f22617c9debdf0bca65fae5
Korchagin <rag.slava@mail.ru>
2022-09-07 20:10:37 +0300

    Добавил тренировчную локацию

---
+++ FREE/Cats/Program.cs
@@ -0,0 +1,17 @@
+﻿string number_string = Console.ReadLine();
+int number = Convert.ToInt32(number_string);
+if (number > 5)
+{
+    if (number < 10)
+    {
+        Console.WriteLine("Соответствующая надпись");
+    }
+    else
+    {
+        Console.WriteLine("Неизвестное число");
+    }
+}
+else
+{
+    Console.WriteLine("Неизвестное число");
+}
---
+++ FREE/Cats2.0/Program.cs
@@ -0,0 +1,9 @@
+﻿double sum = Convert.ToDouble(Console.ReadLine());
+double result = 0;
+if (sum < 100) result = sum + sum * 0.05;
+if (sum > 100 && sum < 200) result = sum + sum * 0.07;
+if (sum > 200) result = sum + sum * 0.1;
+Console.WriteLine("Сумма от вклада в размере ");
+Console.WriteLine(sum);
+Console.WriteLine("равна ");
+Console.WriteLine(result);

ecab5a5b48aa81bc239a024823e5d45961a22cad
Korchagin <rag.slava@mail.ru>
2022-09-23 20:48:20 +0300

    дз 6

---
+++ Homework_6/Homework41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+void quantity(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    Console.Write($"Количество чисел больше нуля = {count}");
+}
+Console.Write("Введите количество чисел, которые хотите ввести = ");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("Вводите числа, которые необходимо проверить");
+int[] arrNumb = new int[M];
+for (int i = 0; i < M; i++)
+{
+    arrNumb[i] = int.Parse(Console.ReadLine());
+}
+quantity(arrNumb);
---
+++ Homework_6/Homework43/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, 
+//y = k2 * x + b2; 
+//значения k1, b1, k2 и b2 задаются пользователем.
+
+double[,] matrixNumbers = new double[2, 2];
+
+for (int row = 0; row < matrixNumbers.GetLength(0); row++)
+{
+    for (int column = 0; column < matrixNumbers.GetLength(1); column++)
+    {
+        matrixNumbers[row, column] = double.Parse(Console.ReadLine());
+    }
+}
+void IntersectionPoint(double[,] matrix)
+{
+    double y;
+    double x;
+    x = (matrix[0, 0] - matrix[1, 0]) / (matrix[1, 1] - matrix[0, 1]);
+    y = matrix[1, 1] * x + matrix[1, 0];
+    Console.WriteLine($"{x}, {y}");
+
+}
+IntersectionPoint(matrixNumbers);

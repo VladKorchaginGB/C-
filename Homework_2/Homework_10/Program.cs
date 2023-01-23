3b8485606851a53ca2f32bb2857f828f48fd1a99
Korchagin <rag.slava@mail.ru>
2023-01-23 19:54:57 +0300

    перове задание

--- Homework_2/Homework_10/Program.cs
+++ Homework_2/Homework_10/Program.cs
@@ -1,10 +1,15 @@
 ﻿//Напишите программу, которая принимает на вход трёхзначное число 
-//и на выходе показывает вторую цифру этого числа.
-int SecondNumber(int num)
+//и на выходе показывает вторую цифру этого числа. 
+//Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+Console.WriteLine("Введите трехзначное число");
+string numString = Console.ReadLine();
+int num = Convert.ToInt32(numString);
+
+int secondDigit(int num)
 {
-    num = num / 10;
-    num = num % 10;
-    return num;
+    int result = num / 10;
+    result = result % 10;
+    return result;
 }
-int test = SecondNumber(486);
-Console.WriteLine(test);
+Console.WriteLine(secondDigit(num));
